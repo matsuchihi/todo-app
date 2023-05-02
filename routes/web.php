58e69f2c1c083b6e356be0fb81e42edebb36d8c0
matsuchihi <cosmosmezon1212@gmail.com>
2023-05-02 17:31:17 +0900

    task create complete

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,9 @@ use App\Http\Controllers\FolderController;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+
 Route::get('/folders/{id}/tasks', [TaskController::class, 'index'])->name('tasks.index');
-Route::get('/folders/create', [FolderController::class,'showCreateForm'])->name('folders.create');
-Route::post('/folders/create', [FolderController::class,'create']);
+Route::get('/folders/create', [FolderController::class, 'showCreateForm'])->name('folders.create');
+Route::post('/folders/create', [FolderController::class, 'create']);
+Route::get('/folders/{id}/tasks/create', [TaskController::class, 'showCreateForm'])->name('tasks.create');
+Route::post('/folders/{id}/tasks/create', [TaskController::class, 'create']);
