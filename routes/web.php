8bc6461d96c8e0a73ed52e8c30e4f72220517b54
matsuchihi <cosmosmezon1212@gmail.com>
2023-05-02 15:59:36 +0900

    create folder complete

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TaskController;
+use App\Http\Controllers\FolderController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,3 +14,5 @@ use App\Http\Controllers\TaskController;
 |
 */
 Route::get('/folders/{id}/tasks', [TaskController::class, 'index'])->name('tasks.index');
+Route::get('/folders/create', [FolderController::class,'showCreateForm'])->name('folders.create');
+Route::post('/folders/create', [FolderController::class,'create']);
